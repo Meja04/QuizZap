83fd75b4cd79c2e920323c5f9d01e752342f7b65
Alfredo <alfredorubino1@gmail.com>
2026-01-30 18:10:37 +0100

    fix prod apiUrl railway

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: '/api', // proxy Nginx per Docker
+  // apiUrl: '/api', // proxy Nginx per Docker
+  apiUrl: 'https://quizzap-backend-production.up.railway.app:8080',
 };

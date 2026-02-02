7df2cc8c361d2300345187d0b6ee8a2a2e84b435
Alfredo <alfredorubino1@gmail.com>
2026-02-02 17:18:34 +0100

    ripristino modifiche per deploy

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  // apiUrl: '/api', // proxy Nginx per Docker
-  apiUrl: '/https://quizzap-json.onrender.com',
+  apiUrl: '/api', // proxy Nginx per Docker
 };
